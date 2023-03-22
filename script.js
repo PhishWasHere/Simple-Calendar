7a8be66ea44bac5a328d0bb70215f4ab6fd074ae
Miran <miran.yasunori00@gmail.com>
2023-03-22 18:13:59 +1100

    added code to add css class based on current time

--- script.js
+++ script.js
@@ -1,101 +1,87 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 
 const timeDisplayEl = $('#currentDay');
 
-function displayTime() {
+function displayTime() { //displays current tiem and date
   const rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
 }
-console.log('hello', timeDisplayEl)
 
-//TO DO:
-// Make header display current time and date [X]
+const timeCurrent = dayjs().format("HH"); //sets current time to 24h time
 
-// Make timeblock editible if clicked on [x]
-//  Add more timeblocks for the hours of business day.
-//   Make save button work and save to local Storage 
-//   add event listener to save button that uploads to local
-
-// when save is clicked, add js to input text content into row []
-
-// Add more time blocks for standart business hours []
-//   make them color coded based on time of dayjs
+console.log('hello', timeCurrent);
 
 
-///////////////////////////
-
-
-
+//probably would have been easier just hard coding the .append into HTML instead of using forEach
 const myDay = [
   {
     id: "0",
     hour: "09",
-    time: "09",
     meridiem: "am",
-    reminder: ""
+    time: '09',
   },
   {
     id: "1",
     hour: "10",
-    time: "10",
     meridiem: "am",
-    reminder: ""
+    time: '10',
   },
   {
     id: "2",
     hour: "11",
-    time: "11",
     meridiem: "am",
-    reminder: ""
+    time: '11',
   },
   {
     id: "3",
     hour: "12",
-    time: "12",
     meridiem: "pm",
-    reminder: ""
+    time: '12',
   },
   {
     id: "4",
     hour: "01",
-    time: "13",
     meridiem: "pm",
-    reminder: ""
+    time: '13',
   },
   {
     id: "5",
     hour: "02",
-    time: "14",
     meridiem: "pm",
-    reminder: ""
+    time: '14',
   },
   {
     id: "6",
     hour: "03",
-    time: "15",
     meridiem: "pm",
-    reminder: ""
+    time: '15',
   },
   {
     id: "7",
     hour: "04",
-    time: "16",
     meridiem: "pm",
-    reminder: ""
+    time: '16',
   },
   {
     id: "8",
     hour: "05",
-    time: "17",
     meridiem: "pm",
-    reminder: ""
+    time: '17',
   },
 
 ]
 
-$(document).ready(function () {
+const reset = '05'; //sets reset time to 5am
+
+function resetPage() { //resets local storage at 5am so you dont need to clear manually
+  if (timeCurrent === reset) {
+    localStorage.clear()
+  }
+}
+
+console.log(timeCurrent)
+
+$(document).ready(function () { //spaghetti code 
   myDay.forEach(function (item, index) {
     $('.append').eq(index).append(
       ` <div id="${item.id}" class="row time-block ">
@@ -105,21 +91,32 @@ $(document).ready(function () {
           <i class="fas fa-save" aria-hidden="true"></i>
         </button>
       </div>`
-    )
+    ) //appending content to each .append
 
-    $(`.append #${item.id} .saveBtn`).on("click", function () {
-      const reminder = $(`.append #${item.id} .description`).val();
+    $(`.append #${item.id} .saveBtn`).on("click", function () { //on click, saves reminders to local storage
+      const reminder = $(`.append #${item.id}`).val();
       localStorage.setItem(item.id, reminder);
     });
 
-    const savedReminder = localStorage.getItem(item.id);
-    if (savedReminder) {
+
+    const rowEl = $(`.append #${item.id}`); //sets class based on current time
+
+    if (timeCurrent == parseInt(item.time)) {
+      rowEl.addClass("present");  console.log('current')
+    } else if (timeCurrent > parseInt(item.time)) { 
+      rowEl.addClass("past"); console.log('past')
+    } else {
+      rowEl.addClass("future");
+      console.log('future')
+    }
+
+    const savedReminder = localStorage.getItem(item.id); 
+    if (savedReminder) { //checks if savedReminder has a value, if it does it runs the command
       $(`.append #${item.id} .description`).val(savedReminder);
     }
   });
 });
 
 
-
-displayTime()
-setInterval(displayTime, 1000);
+displayTime() // displays time
+setInterval(displayTime, 1000); //updates every 1s
